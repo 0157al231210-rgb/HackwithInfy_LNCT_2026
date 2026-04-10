9f84dc70708ac08048bd3d34971fee9c0e0dfd81
Swaraj Sahay Verma <0157al231210@lnct.ac.in>
2026-04-10 07:33:46 +0530

    10/4/26

---
+++ DP/983.py
@@ -0,0 +1,17 @@
+class Solution:
+    def mincostTickets(self, days: List[int], costs: List[int]) -> int:
+        dp = {len(days):0}
+
+        def dfs(i):
+            if i in dp:
+                return dp[i]
+            
+            dp[i] = float("inf")
+            for cost, duration in zip(costs , [1,7,30]):
+                j = i
+                while j < len(days) and days[j] < days[i] + duration:
+                    j += 1
+                dp[i] = min(dp[i], cost + dfs(j))
+
+            return dp[i]   
+        return dfs(0)
